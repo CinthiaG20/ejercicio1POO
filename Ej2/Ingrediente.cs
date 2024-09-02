48cd5bfdc077e571afd71098968ddeb677e95145
CinthiaG20 <cg9150153@gmail.com>
2024-09-02 17:51:56 -0300

    a

---
+++ Ej2/Ingrediente.cs
@@ -0,0 +1,20 @@
+namespace Ej2;
+
+public class Ingrediente
+{
+    private string nombre;
+
+    public string Nombre
+    {
+        get { return nombre; }
+        set { nombre = value; }
+    }
+
+    private double costo;
+
+    public double Costo
+    {
+        get { return costo; }
+        set { costo = value; }
+    }
+}
---
+++ Ej2/Smoothie.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+
+namespace Ej2;
+
+public class Smoothie
+{
+    private ArrayList ingredientes = new ArrayList();
+    private string nombre;
+    public ArrayList AddIngredient(Ingrediente ing)
+    {
+        if (!ingredientes.Contains(ing))
+        {
+            ingredientes.Add(ing);
+        }
+
+        return ingredientes;
+    }
+
+    public double GetTotalPrice()
+    {
+        double suma = 0;
+        foreach (Ingrediente VAR in ingredientes)
+        {
+            double precio=VAR.Costo;
+            suma += precio;
+        }
+
+        return suma;
+    }
+
+    public string GetFullName()
+    {
+        string texto = "";
+        foreach (Ingrediente VAR in ingredientes)
+        {
+            texto += VAR.Nombre+" ";
+        }
+
+        return texto;
+    }
+}
